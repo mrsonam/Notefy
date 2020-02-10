38db06c688a55f8caaf164023bae3c1825dcb3d3
mrsonam <53036054+mrsonam@users.noreply.github.com>
2020-02-10 19:19:14 +0545

    Notes

--- Notefy/notes/forms.py
+++ Notefy/notes/forms.py
@@ -5,7 +5,7 @@ class NoteForm(forms.ModelForm):
     note_password = forms.CharField(required=False, widget=forms.PasswordInput())
     class Meta:
         model = Note
-        fields = ('note_title','note_content', 'note_images','note_videos','note_password' )
+        fields = ('note_title','note_content', 'note_images', 'note_audios', 'note_videos','note_password' )
 
 class note_access(forms.Form):
     note_password = forms.CharField(widget=forms.PasswordInput())
--- Notefy/notes/models.py
+++ Notefy/notes/models.py
@@ -9,6 +9,7 @@ class Note(models.Model):
     note_title = models.CharField(max_length=20)
     note_content = models.TextField()
     note_images = models.ImageField(upload_to='images', null=True, blank = True)
+    note_audio = models.FileField(upload_to='audios', null=True, blank = True)
     note_videos = models.FileField(upload_to='videos', null=True, blank = True)
     note_password = models.CharField(max_length=50, null=True, blank = True)
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -18,6 +19,7 @@ class Note(models.Model):
 
     def delete(self, *args, **kwargs):
         self.note_images.delete()
+        self.note_audios.delete()
         self.note_videos.delete()
         super(Note, self).delete(*args, **kwargs)
 
